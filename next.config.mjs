1385a95283eea9503569121372d23093489994f5
Faizan <faizansmusic@gmail.com>
2025-12-05 17:02:29 +0530

    pwa

--- next.config.mjs
+++ next.config.mjs
@@ -11,12 +11,25 @@ const withPWA = withPWAInit({
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
+import withPWAInit from "next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
 };
 
-const isDev = process.env.NODE_ENV === "development";
+const isDev = process.env.NODE_ENV === "development" || process.argv.join(" ").includes("dev");
 
 export default isDev ? nextConfig : withPWA(nextConfig);
